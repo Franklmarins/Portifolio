4217807bf3455d3d6b3c9c6cd9b2aac218622e6d
franklmarins <franklmarins@gmail.com>
2023-05-17 15:09:07 -0300

    feat: user config

--- src/components/Project/index.tsx
+++ src/components/Project/index.tsx
@@ -31,6 +31,34 @@ export const Project = (): JSX.Element => {
       );
 
       const json = await data.json();
+      async function getLanguages(
+        userData: {
+          nameUser?: string;
+          githubUser: any;
+          linkedinUser?: string;
+          whatsappNumber?: string;
+          emailUser?: string;
+          instagramUser?: string;
+          facebookUser?: string;
+        },
+        json: { name: any; language: string[] }[]
+      ) {
+        const languages = await Promise.all(
+          json.map(async (project: { name: any; language: string[] }) => {
+            const response = await fetch(
+              `https://api.github.com/repos/${userData.githubUser}/${project.name}/languages`
+            );
+
+            const data = await response.json();
+
+            project.language = Object.keys(data);
+          })
+        );
+
+        return languages;
+      }
+
+      await getLanguages(userData, json);
 
       setRepositories(json);
 
@@ -43,7 +71,7 @@ export const Project = (): JSX.Element => {
   return (
     <>
       {repositories &&
-        repositories?.map?.((repository) => (
+        repositories?.map?.((repository: any) => (
           <ProjectWrapper key={repository.id}>
             <ProjectTitle
               as="h2"
@@ -56,21 +84,15 @@ export const Project = (): JSX.Element => {
 
             <ProjectStack>
               <Text type="body2" color="grey2">
-                Primary Language:
+                Linguagens utilizadas:
               </Text>
-              {repository.language ? (
-                <ProjectStackTech>
+              {repository.language.map((e: string) => (
+                <ProjectStackTech key={Math.random()}>
                   <Text color="grey2" type="body2">
-                    {repository.language}
+                    {e}
                   </Text>
                 </ProjectStackTech>
-              ) : (
-                <ProjectStackTech>
-                  <Text color="grey2" type="body2">
-                    Primary language not identified
-                  </Text>
-                </ProjectStackTech>
-              )}
+              ))}
             </ProjectStack>
 
             <Text type="body1" color="grey2">
@@ -78,14 +100,11 @@ export const Project = (): JSX.Element => {
             </Text>
             <ProjectLinks>
               <ProjectLink target="_blank" href={repository.html_url}>
-                <FaGithub /> Github Code
+                <FaGithub /> Código no Github
               </ProjectLink>
               {repository.homepage && (
-                <ProjectLink
-                  target="_blank"
-                  href={`https://${repository.homepage}`}
-                >
-                  <FaShare /> See demo
+                <ProjectLink target="_blank" href={`${repository.homepage}`}>
+                  <FaShare /> Ver demonstração
                 </ProjectLink>
               )}
             </ProjectLinks>
--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Franklin Marins",
+  githubUser: "Franklmarins",
+  linkedinUser: "franklin-marins",
+  whatsappNumber: "+5521996372647",
+  emailUser: "franklmarins@gmail.com",
+  instagramUser: "",
   facebookUser: "",
 };
